10da41d93c964db768d596c4953ce2b8aded8ad5
Mark Bruderer <markbruderer123@gmail.com>
2024-11-12 18:31:15 +0100

    parse the meta of nuxt ui.

--- template/nuxt-ui/parseMeta.ts
+++ template/nuxt-ui/parseMeta.ts
@@ -1,12 +1,12 @@
 import * as fs from 'node:fs'
 import matter from 'gray-matter'
 
-const componentMds = fs.readdirSync('template/nuxt-ui/.repo/docs/content/3.components').filter(file => file.endsWith('.md'))
+const componentMds = fs.readdirSync('template/nuxt-ui/.repo/docs/content/2.components').filter(file => file.endsWith('.md'))
 
 const components = componentMds
   .map((file) => {
     const componentName = file.split('.')[0]
-    const { content, data: frontmatter } = matter(fs.readFileSync(`template/shadcn-vue/.repo/content/docs/components/${file}`, 'utf-8'))
+    const { content, data: frontmatter } = matter(fs.readFileSync(`template/nuxt-ui/.repo/docs/content/2.components/${file}`, 'utf-8'))
     const usage = content.split(`## Usage`)[1]?.split(`##`)[0]?.trim().replace('```vue\n', '').replace('```', '')
 
     return {
