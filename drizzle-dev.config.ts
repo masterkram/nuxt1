dcfd2b235fb67718c6ae7d887cdfa5603914df86
Mark Bruderer <markbruderer123@gmail.com>
2025-01-11 17:38:06 +0100

    add dev into drizzle dev config.

--- drizzle-dev.config.ts
+++ drizzle-dev.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
   out: 'server/database/migrations',
   schema: 'server/database/schema.ts',
   driver: 'better-sqlite',
-  dialect: 'turso',
   dbCredentials: {
-    url: process.env.TURSO_DB_URL!,
-    authToken: process.env.TURSO_DB_TOKEN!,
+    url: join(process.cwd(), './db.sqlite'),
   },
 })
